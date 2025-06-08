3cf43f0623079141df7f4cee533355a33f0f1834
ChethanL07 <144891137+ChethanL07@users.noreply.github.com>
2025-06-08 23:59:40 +0530

    initial commit

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { DiHtml5, DiCss3, DiJavascript1, DiReact, DiPython } from "react-icons/di";
+import { SiDjango } from "react-icons/si";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiDjango />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
